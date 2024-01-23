de3276d3e076c3560b2f5c7f4f03156dd534e381
sewerynstawiarski <seweryn.stawiarski@gmail.com>
2024-01-23 21:41:34 +0100

    MySQL - start = query/insert

---
+++ Databases/QueryMusic/src/lpa/Main.java
@@ -0,0 +1,71 @@
+package lpa;
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        try {
+            props.load(Files.newInputStream(Path.of("music_properties"), StandardOpenOption.READ));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        var dataSource = new MysqlDataSource();
+        dataSource.setServerName(props.getProperty("serverName"));
+        dataSource.setPort(Integer.parseInt(props.getProperty("port")));
+        dataSource.setDatabaseName(props.getProperty("databaseName"));
+        try {
+            dataSource.setMaxRows(10);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        String query = "SELECT * FROM music.artists limit 10";
+//        String query = """
+//                WITH RankedRows AS (
+//                                    SELECT *,
+//                                    ROW_NUMBER() OVER (ORDER BY artist_id) AS row_num
+//                                    FROM music.artists
+//                                    )
+//                                    SELECT *
+//                                            FROM RankedRows
+//                                    WHERE row_num <= 10""";
+
+
+
+        try(var connection = dataSource.getConnection(props.getProperty("user"), System.getenv("MYSQL_PASS"));
+            Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery(query);
+
+            var meta = resultSet.getMetaData();
+
+            System.out.println("*".repeat(70));
+            for (int i = 1; i <= meta.getColumnCount(); i++) {
+                System.out.printf("%-15s", meta.getColumnName(i).toUpperCase());
+            }
+            System.out.println();
+            while (resultSet.next()) {
+//                System.out.printf("%d %s %s %n", resultSet.getInt("track_number"),
+//                        resultSet.getString("artist_name"),
+//                        resultSet.getString("song_title"));
+                for (int i = 1; i <= meta.getColumnCount(); i++) {
+                    System.out.printf("%-15s", resultSet.getString(i));
+                }
+                System.out.println();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ Debugging_Section/MySQLMusic/src/lpa/Main.java
@@ -0,0 +1,2 @@
+package lpa;public class Main {
+}
