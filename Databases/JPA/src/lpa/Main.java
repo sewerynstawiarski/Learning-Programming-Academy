3304cf9f2fcf5d8a75a170c51da0afe5ae6fd6c5
sewerynstawiarski <seweryn.stawiarski@gmail.com>
2024-04-08 21:31:33 +0200

    JPa and JPQL query

---
+++ Databases/JPA/src/lpa/Main.java
@@ -0,0 +1,29 @@
+package lpa;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Persistence;
+import lpa.music.Artist;
+
+public class Main {
+    public static void main(String[] args) {
+        try(var sessionFactory = Persistence.createEntityManagerFactory("lpa.music");
+            EntityManager entityManager = sessionFactory.createEntityManager();
+        ) {
+            var transaction = entityManager.getTransaction();
+            transaction.begin();
+            Artist artist = entityManager.find(Artist.class, 202);
+//            Artist artist = new Artist(202, "Muddy Water");
+//            artist.setArtistName("Muddy Waters");
+            System.out.println(artist);
+//            artist.removeDuplicates();
+            artist.addAlbum("The Best Of Muddy Waters");
+            System.out.println(artist);
+//            entityManager.remove(artist);
+//            entityManager.persist(new Artist("Muddy Water"));
+            entityManager.merge(artist);
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
