001e84af60e8f59dea85e9ef802aaa0f833c16f9
sewerynstawiarski <seweryn.stawiarski@gmail.com>
2024-08-13 13:24:42 +0200

    about URL, URI

---
+++ HttpProject/src/lpa/URIBasics.java
@@ -0,0 +1,70 @@
+package lpa;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+public class URIBasics {
+    public static void main(String[] args) {
+        URI baseSite = URI.create("https://learningprogramming.academy");
+//        URI timsSite = URI.create("https://learningprogramming.academy/courses/complete-java-masterclass");
+        URI timsSite = URI.create("courses/complete-java-masterclass");
+        print(timsSite);
+
+        try {
+            URI second = new URI("http://user:pw@store.com:5000/products/phones?os=android#samsung");
+            print(second);
+
+            URI masterClass = baseSite.resolve(timsSite);
+            URL url = masterClass.toURL();
+            System.out.println(masterClass);
+            print(url);
+
+        } catch (URISyntaxException | MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+    private static void print (URI uri) {
+        System.out.printf("""
+                --------------------------------------
+                [scheme:]scheme-specific-part[#fragment]
+                ---------------------------------------
+                Scheme: %s
+                Scheme-specific part: %s
+                    Authority: %s
+                        User info: %s
+                        Host: %s
+                        Port: %d
+                        Path: %s
+                        Query: %s
+                    Fragment: %s %n""",
+                uri.getScheme(),
+                uri.getSchemeSpecificPart(),
+                uri.getAuthority(),
+                uri.getUserInfo(),
+                uri.getHost(),
+                uri.getPort(),
+                uri.getPath(),
+                uri.getQuery(),
+                uri.getFragment());
+    }
+    private static void print (URL url) {
+        System.out.printf("""
+                --------------------------------------
+                    Authority: %s
+                        User info: %s
+                        Host: %s
+                        Port: %d
+                        Path: %s
+                        Query: %s %n""",
+                url.getAuthority(),
+                url.getUserInfo(),
+                url.getHost(),
+                url.getPort(),
+                url.getPath(),
+                url.getQuery());
+    }
+}
